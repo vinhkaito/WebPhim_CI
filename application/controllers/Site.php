1efa4492732daef006dfe5de1d54a94738f84044
vinh <vinh@bliss-interactive.com>
2019-02-26 18:47:36 +0700

    load view index

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -10,6 +10,6 @@ class Site extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('welcome_message');
+        $this->load->view('index');
     }
 }
---
+++ application/views/index.php
@@ -0,0 +1,11 @@
+<?php
+$this->load->view('modules/header');
+?>
+<main class="container">
+    <?php
+    $this->load->view('modules/home');
+    ?>
+</main>
+<?php
+$this->load->view('modules/footer');
+?>
