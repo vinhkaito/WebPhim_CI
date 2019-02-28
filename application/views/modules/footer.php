433a71516eb4bddb54c7a582aad54c482c3d8a8d
vinh <vinh@bliss-interactive.com>
2019-02-28 19:40:59 +0700

    up

--- application/views/modules/footer.php
+++ application/views/modules/footer.php
@@ -1,5 +1,18 @@
-<footer>
+<footer class="footer">
     <div class="container">
-
+        <div class="clearfix">
+            <div class="row">
+                <div class="col-sm-9 col-md-9 col-lg-9">
+                    <span class="text-muted">Mọi dữ liệu trên WebPhimCI đều được tổng hợp từ internet.</span>
+                    <hr class="mt-2 mb-2">
+                    <span class="text-muted">&copy; <strong>2019</strong> All rights reserved.</span>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3">
+                    <div class="text-muted d-block"><i class="fa fa-facebook-square"></i> <a class="text-muted" href="#"> Facebook</a></div>
+                    <div class="text-muted d-block"><i class="fa fa-phone-square"></i> <a class="text-muted" href="#"> Contact</a></div>
+                    <div class="text-muted d-block"><i class="fa fa-instagram "></i> <a class="text-muted" href="#"> Instagram</a></div>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
