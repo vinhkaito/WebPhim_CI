f49bbe7b5cf73d39b6f927f0d4a4a41f6b34221f
Huy <Khanh>
2019-03-13 09:16:03 +0700

    up

--- application/views/page/infomation.php
+++ application/views/page/infomation.php
@@ -2,8 +2,12 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-9">
-                <div>
-                    //code o day
+                <div class="title">
+                    <h5><i class="fa fa-clock-o"></i> Xem Phim</h5>
+                    <hr>
+                    <b style="color: white">LOGO Phim</b></br>
+                    <img class="anime-thumbnail" src="<?= base_url() ?>./webroot/images/Meiji-Tokyo-Renka.jpg" alt="Meiji Tokyo Renka" height="450px" width="100px"></br>
+                    <a href="#"><input type="button" name="" value="<" ></a><a href="#"><input type="button" name="" value=">"></a><a href="#"><input type="button" name="" value="Request"></a><h>
                 </div>                        
             </div>
             <div class="col-lg-3 t-w">
